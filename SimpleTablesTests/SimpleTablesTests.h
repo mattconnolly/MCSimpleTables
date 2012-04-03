c8c7880a6beb78d9ff033c38044493cbc87ad018
Matt Connolly <matt.connolly@me.com>
2012-04-03 21:12:28 +1000

    Created SimpleTests static library project

---
+++ SimpleTablesTests/SimpleTablesTests.h
@@ -0,0 +1,13 @@
+//
+//  SimpleTablesTests.h
+//  SimpleTablesTests
+//
+//  Created by Matt Connolly on 3/04/12.
+//  Copyright (c) 2012 Sound Evolution Pty Ltd. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SimpleTablesTests : SenTestCase
+
+@end
---
+++ SimpleTablesTests/SimpleTablesTests.m
@@ -0,0 +1,32 @@
+//
+//  SimpleTablesTests.m
+//  SimpleTablesTests
+//
+//  Created by Matt Connolly on 3/04/12.
+//  Copyright (c) 2012 Sound Evolution Pty Ltd. All rights reserved.
+//
+
+#import "SimpleTablesTests.h"
+
+@implementation SimpleTablesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SimpleTablesTests");
+}
+
+@end
