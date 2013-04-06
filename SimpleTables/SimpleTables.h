bd9cc35ba0fe4ce7cef9c0ff0b20c386a2cf3103
Matt Connolly <matt.connolly@me.com>
2013-04-06 23:01:18 +1000

    Adding a unified header file

---
+++ SimpleTables/SimpleTables.h
@@ -0,0 +1,22 @@
+//
+//  SimpleTables.h
+//  SimpleTables
+//
+//  Created by Matt Connolly on 6/04/13.
+//  Copyright (c) 2013 Sound Evolution Pty Ltd. All rights reserved.
+//
+
+#ifndef SimpleTables_SimpleTables_h
+#define SimpleTables_SimpleTables_h
+
+#ifdef __OBJC__
+
+#import <SimpleTables/SimpleTableViewController.h>
+#import <SimpleTables/SimpleTableSection.h>
+#import <SimpleTables/SimpleTableCell.h>
+#import <SimpleTables/SimpleTableTextEditCell.h>
+#import <SimpleTables/SimpleTableFooterLabel.h>
+
+#endif
+
+#endif
